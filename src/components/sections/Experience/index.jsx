5bbd1c4434fe3658cf3b2a5055c719351da6a414
Ana <anapribbeiro@gmail.com>
2026-02-17 17:27:32 -0300

    feat: update Contact and Experience sections with improved layout and additional components

--- src/components/sections/Experience/index.jsx
+++ src/components/sections/Experience/index.jsx
@@ -1,10 +1,12 @@
 import ExperienceCard from "./ExperienceCard";
 import Title from "../../Title";
 import experiences from "../../../data/experience";
+import Button from "../../ui/Button";
+import { SiLinkedin } from "react-icons/si";
 
 export default function Experience() {
   return (
-    <section id="experience" className="py-8 bg-gradiente">
+    <section id="experience" className="py-8 bg-gradiente h-screen">
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           <Title
@@ -16,6 +18,16 @@ export default function Experience() {
             {experiences.map((experiences, index) => (
               <ExperienceCard key={index} exp={experiences} />
             ))}
+          </div>
+
+          <div className="flex justify-center items-center mt-20">
+            <Button
+              href="https://www.linkedin.com/in/anamartinsr/"
+              icon={SiLinkedin}
+              className="mt-8"
+            >
+              Ver minha jornada no desenvolvimento
+            </Button>
           </div>
         </div>
       </div>
