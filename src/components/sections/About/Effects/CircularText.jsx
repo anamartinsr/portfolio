b0e20fc9c1797c9e7c734c649b18a76c39f9bf88
Ana Paula Martins <anapribbeiro@gmail.com>
2026-04-15 17:15:10 -0300

    style: update className in CircularText component to use correct color variable syntax

--- src/components/sections/About/Effects/CircularText.jsx
+++ src/components/sections/About/Effects/CircularText.jsx
@@ -85,7 +85,7 @@ const CircularText = ({
 
   return (
     <motion.div
-      className={`m-0 mx-auto rounded-full w-[200px] h-[200px] relative text-(--circular-text) font-black text-center cursor-pointer origin-center ${className}`}
+      className={`m-0 mx-auto rounded-full w-[200px] h-[200px] relative text-[color:var(--color-circular-text)] font-black text-center cursor-pointer origin-center ${className}`}
       style={{ rotate: rotation }}
       initial={{ rotate: 0 }}
       animate={controls}
